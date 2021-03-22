51354d03f6515da2b5bd5b69e0b597864daf2e4c
starscream202 <starscream202@gmail.com>
2021-03-21 19:36:04 -0500

    pipelineTest

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,25 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 600; i++) {
-            System.out.println("Magic");
+        String sentence="i love him and i am doing this for the graham";
+        System.out.println(sentence);
+        String hashTag="#";
+        String [] strings=sentence.split(" ");
+
+        List<String> strings1=new ArrayList<>();
+
+        for (String words :strings) {
+            String modifiedText=hashTag+words;
+            strings1.add(modifiedText);
+
+        }
+        for(int i=0;i<strings1.size();i++){
+            System.out.print(strings1.get(i)+" ");
         }
     }
 }
